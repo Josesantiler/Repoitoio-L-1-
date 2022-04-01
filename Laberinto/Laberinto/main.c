1277386a7a35f398751964a8b093e9da4a78a72e
Josesantiler <99481291+Josesantiler@users.noreply.github.com>
2022-04-01 13:25:47 +0200

    Cambio de sincronizacion con xcode
    es asi

---
+++ Laberinto/Laberinto/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Laberinto
+//
+//  Created by Jose Miguel Santiler Gomez on 1/4/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
